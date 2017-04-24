59bc7e1c582ffaffda85a2d6eff4e8b9b59d0865
kunkuntang <kuntang@163.com>
2017-04-25 02:59:52 +0800

    add the material framework

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+injectTapEventPlugin();
+
+const App = () => (
+  <MuiThemeProvider>
+    <MyAwesomeReactComponent />
+  </MuiThemeProvider>
+);
+ 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('app')
 );
+
+
+// ReactDOM.render(
+//   <App />,
+//   <Material />,
+//   document.getElementById('root')
+// );
